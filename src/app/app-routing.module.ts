1b89f4be3c34990b9b71a6f2d667505286b4c4fa
Tran-Anh-Duc <trananhducty@gmail.com>
2021-12-24 01:36:38 +0700

    van bi loi chua fix duoc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductListComponent} from "./product-list/product-list.component";
+import {ProductCreateComponent} from "./product/product-create/product-create.component";
+import {ProductDeleteComponent} from "./product/product-delete/product-delete.component";
+import {ProductEditComponent} from "./product/product-edit/product-edit.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'product/list',
+  component:ProductListComponent,
+},{
+  path:'product/create',
+  component:ProductCreateComponent
+},{
+  path:'product/list/delete/:id',
+  component:ProductDeleteComponent
+},{
+  path:'product/list/edit/:id',
+  component:ProductEditComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ProductService} from "./service/product.service";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductCreateComponent } from './product/product-create/product-create.component';
+import { ProductDeleteComponent } from './product/product-delete/product-delete.component';
+import { ProductEditComponent } from './product/product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductCreateComponent,
+    ProductDeleteComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
